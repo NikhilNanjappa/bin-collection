9a66a14d4a26725f84eb385537917dadd479ea6e
Nikhil Nanjappa <94963727+NikhilNanjappa@users.noreply.github.com>
2023-07-30 18:35:55 +0100

    Removed favicon from static

--- src/server/middlewares/configure-static-files.js
+++ src/server/middlewares/configure-static-files.js
@@ -4,7 +4,6 @@ import favicon from 'serve-favicon';
 export default (app) => {
   app.use('/images/favicon.ico', express.static('node_modules/govuk-frontend/govuk/assets/images/favicon.ico'));
   app.use('/favicon.ico', express.static('node_modules/govuk-frontend/govuk/assets/images/favicon.ico'));
-  app.use(favicon('node_modules/govuk-frontend/govuk/assets/images/favicon.ico'));
   app.use('/public', express.static('src/public/'));
   app.use('/assets', express.static('node_modules/govuk-frontend/govuk/assets/'));
   app.use('/govuk-frontend', express.static('node_modules/govuk-frontend/govuk'));
